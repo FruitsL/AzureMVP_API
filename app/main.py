f747f56e32a7528221a283d810694e3ff97f8d79
FruitsL <sa4560@naver.com>
2025-09-29 11:37:20 +0900

    app insight 추가

--- app/main.py
+++ app/main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from datetime import datetime
+
 import logging
+import os
+from opencensus.ext.azure.log_exporter import AzureLogHandler
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+# Application Insights 연결 문자열 환경변수 사용
+connection_string = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING", "")
+if connection_string:
+    logger.addHandler(AzureLogHandler(connection_string=connection_string))
 
 app = FastAPI()
 
